1ba9472872a82096c7689dc5e5a39f5e7eb2b0fe
angelloz <scottloz@protonmail.com>
2022-10-30 16:19:50 +0000

    first commit

---
+++ php/connect.php
@@ -0,0 +1,11 @@
+<?php
+$domain = "bytebuilder.org/";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname='shortenURL';
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
